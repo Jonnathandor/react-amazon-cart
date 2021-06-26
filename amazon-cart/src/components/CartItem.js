574642b2440f4789ef51fa5f3f7d97e8bfaa8021
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-06-26 12:11:24 -0500

    added props to CartItem component

--- amazon-cart/src/components/CartItem.js
+++ amazon-cart/src/components/CartItem.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import './CartItem.css'
 
-export default function CartItem() {
+export default function CartItem({ title, stock, image, price, quantity }) {
     return (
         <div className="cart-item">
             <div className="cart-item-image">
-                <img alt="product" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpvKlHOlYcOuachLvJEXeLSer4E5iBJx87dVcSjawLcbG3pVDV483nOG4Xog&usqp=CAc"></img>
+                <img alt="product" src={process.env.PUBLIC_URL + './items/' + image}></img>
             </div>
             <div className="cart-item-info">
                 <div className="info-title">
-                    <h2>Ipad Pro</h2>
+                    <h2>{title}</h2>
                 </div>
                 <div className="info-stock">
-                    In Stock
+                    {stock}
                 </div>
                 <div className="item-actions">
                     <div className="item-quantity">
-                        <select>
+                        <select value={quantity}>
                             <option value="1">Qty: 1</option>
-                            <option value="1">Qty: 2</option>
-                            <option value="1">Qty: 3</option>
+                            <option value="2">Qty: 2</option>
+                            <option value="3">Qty: 3</option>
                         </select>
                     </div>
                     <div className="item-actions-divider">|</div>
@@ -29,7 +29,7 @@ export default function CartItem() {
                 </div>
             </div>
             <div className="cart-item-price">
-                $769.00
+                ${price}
             </div>
         </div>
     );
--- amazon-cart/src/components/CartItems.js
+++ amazon-cart/src/components/CartItems.js
@@ -4,13 +4,23 @@ import CartItem from './CartItem';
 
 
 export default function CartItems({ items }) {
-  console.log('inside cartItem ', items);
   return (
       <div className="cart-items">
         <h1>Shopping Cart</h1>
         <hr></hr>
         <div className="cart-items-list">
-          <CartItem/>
+          {
+            items.map((item, index) => {
+              return <CartItem
+              key={index}
+              title={item.title}
+              stock={item.stock}
+              image={item.image}
+              price={item.price}
+              quantity={item.quantity}
+              />
+            })
+          }
         </div>
       </div>
   );
