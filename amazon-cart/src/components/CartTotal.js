9d4bf86561d6a662edb1f920236a92af82ddd31c
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-06-26 13:44:46 -0500

    added cart total style

--- amazon-cart/src/components/CartTotal.js
+++ amazon-cart/src/components/CartTotal.js
@@ -4,7 +4,10 @@ import './CartTotal.css';
 export default function CartTotal() {
     return (
         <div className="cart-total">
-
+            <h3>Subtotal <span className="cart-total-price">$2000</span></h3>
+            <button>
+                Proceed to Checkout
+            </button>
         </div>
     );
 }
