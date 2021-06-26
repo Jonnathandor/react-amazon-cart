8ae251f6ebd812b69c0dbc6d21a0e8fdbe0ffb9a
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-06-26 13:55:21 -0500

    added a function that calculates teh total price

--- amazon-cart/src/components/CartTotal.js
+++ amazon-cart/src/components/CartTotal.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './CartTotal.css';
 
-export default function CartTotal() {
+export default function CartTotal({items}) {
+    const getTotalPrice = () => {
+        let total = 0;
+        items.forEach(item => {
+            total += item.price * item.quantity;
+        });
+
+        return total;
+    }
     return (
         <div className="cart-total">
-            <h3>Subtotal <span className="cart-total-price">$2000</span></h3>
+            <h3>Subtotal({items.length} items): <span className="cart-total-price">${getTotalPrice()}</span>
+            </h3>
             <button>
                 Proceed to Checkout
             </button>
